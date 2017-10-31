aa3cfcabeba37bb1d2a99f1f863ff3633c51d805
rbb3 <rbb3@cin.ufpe.br>
2017-10-30 23:13:30 -0300

    fix(solutionD)

--- D/solucao.cpp
+++ D/solucao.cpp
@@ -12,10 +12,10 @@ int main() {
   for(int i =0 ; i < s.size(); i++) {
     if (s[i] == ' ') continue;
     if (s[i] >= 'a' && s[i] <= 'z') {
-      if (N + s[i] > 'z') s[i] = 'a' + N + s[i] - 'z';
+      if (N + s[i] > 'z') s[i] = 'a' + N + s[i] - 'z' - 1;
       else s[i] += N;
     } else {
-      if (N + s[i] > 'Z') s[i] = 'A' + N + s[i] - 'Z';
+      if (N + s[i] > 'Z') s[i] = 'A' + N + s[i] - 'Z' - 1;
       else s[i] += N;
     }
   }
